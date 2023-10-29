13873da01edd648278d6868d6aaed091a8d4ba25
lekhanhlinh-star <82929088+lekhanhlinh-star@users.noreply.github.com>
2023-10-29 17:47:27 +0700

    add-docker

--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,7 @@ const middelwaresAuth=require("../middlewares/verifyToken")
 const LoginUser=require("../controllers/login")
 route.post("/v1/api/user",UserController.create_user);
 route.get("/v1/api/user",UserController.get_all_users);
-route.get("/v1/api/user/:id",UserController.get_user_by_id)
+route.get("/v1/api/user/:id", middelwaresAuth.verifyToken,UserController.get_user_by_id)
 route.delete("/v1/api/user/:id",UserController.delete_user_by_id)
 route.put("/v1/api/user/:id",UserController.update_user)
 route.post("/v1/api/user/login",LoginUser.login)
